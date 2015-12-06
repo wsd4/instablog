199a512185aa65718e68280d5955e326c5356726
Myeonggu Sa <quest4i@me.com>
2015-12-06 23:59:14 +0900

    week3 [model] Category 추가

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from .models import Post
 from .models import Tag
 from .models import Comment
+from .models import Category
 
 
 class CommentInlineAdmin(admin.TabularInline):
@@ -30,6 +31,17 @@ class TagAdmin(admin.ModelAdmin):
     search_fields = ('name', )
     date_hierarchy = 'created_at'
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at', )
+    list_display_links = ('id', 'name', )
+    list_filter = ('created_at', )
+    search_fields = ('name', )
+    date_hierarchy = 'created_at'
+
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Comment)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -37,5 +37,13 @@ class Tag(models.Model):
         return '<Tag {}: "{}">'.format(self.pk, self.name)
 
 
+class Category(models.Model):
+
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '<Category {}: "{}">'.format(self.pk, self.name)
 
 
