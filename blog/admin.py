0e7b0d4149340fc588e06bd8abd4636d57e9176f
Myeonggu Sa <quest4i@me.com>
2015-12-06 23:55:17 +0900

    week3 [model] Post, Comment, Tag 관리 기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Post
+from .models import Tag
+from .models import Comment
+
+
+class CommentInlineAdmin(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'title', 'created_at', )
+    list_display_links = ('id', 'title', )
+    ordering = ('-id', '-created_at', )
+    inlines = [CommentInlineAdmin]
+    list_filter = ('tags', )
+    search_fields = ('content', )
+    date_hierarchy = 'created_at'
+
+
+class TagAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'created_at')
+    list_display_links = ('id', 'name', )
+    list_filter = ('created_at', )
+    search_fields = ('name', )
+    date_hierarchy = 'created_at'
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,12 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField('Tag')
 
+    class Meta:
+        ordering = ('-created_at', '-pk')
+
+    def __str__(self):
+        return '<Post {}: "{}">'.format(self.pk, self.title[:8])
+
 
 class Comment(models.Model):
 
@@ -17,7 +23,19 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return '<Comment {}: {} - "{}">'.format(self.pk, self.post.title, self.content)
+
 
 class Tag(models.Model):
 
     name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '<Tag {}: "{}">'.format(self.pk, self.name)
+
+
+
+
